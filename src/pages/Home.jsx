ecf8478f0b51cc3801ea0b26fa432b20e155e2aa
Silvino Gomes <silvinog97@gmail.com>
2025-03-17 22:55:50 +0100

    consumindo os dados da api, fazendo testes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+/*useState pra poder gerenciar os estados dos filmes */
+/*useEffect pra gente poder fazer a chamada da API quando a página carregar.*/
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 
 const Home = () => {
+    const [topMovies, setTopMovies] = useState([]); /*Gerenciando os estados dos nossos melhoresFilmes */
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        console.log(data);
+    };
+
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl);
+    }, []);
+
+
   return (
     <div>Home</div>
   )
-}
+};
 
 export default Home
